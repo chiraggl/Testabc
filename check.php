299aafd383e9dd81edc23371dab75f6028079886
Chirag Goel <chiraggoel.it@gmail.com>
2023-08-31 17:47:10 +0200

    Update check.php

--- check.php
+++ check.php
@@ -1,2 +1,7 @@
-$filename = $_GET["filename"];
-include $filename; // Noncompliant
+namespace Foo\Bar;
+
+try {
+    doSomething();
+} catch (Exception $e) { // Noncompliant - Exception will never be caught because the class Exception does not exist in the namespace
+    echo $e->message;
+}
