cdab3d33c73e615295eafa76eebbb9699b3ed363
Chirag Goel <chiraggoel.it@gmail.com>
2023-10-22 11:04:19 +0200

    Update check.php

--- check.php
+++ check.php
@@ -1,5 +1,21 @@
 echo '<h1>' . $input . '</h1>';
 
-This is a PrivateData_Code
+#This is a PrivateData_Code
 
-Testing the repository
+#Testing the repository
+
+if (PHP_SAPI === 'cli') {
+    parse_str(implode('&', array_slice($argv, 1)), $_GET);
+}
+
+$file_db = new PDO('sqlite:../database/database.sqlite');
+
+if (NULL == $_GET['id']) $_GET['id'] = 1;
+
+$sql = 'SELECT * FROM employees WHERE employeeId = ' . $_GET['id'];
+
+foreach ($file_db->query($sql) as $row) {
+    $employee = $row['LastName'] . " - " . $row['Email'] . "\n";
+
+    echo $employee;
+}
